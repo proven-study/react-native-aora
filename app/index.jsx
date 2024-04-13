155c08a15ddb705df9a8d2e60d1737b94eebb96f
Md Arif Hossain <hi@devarif.me>
2024-04-13 19:31:43 +0600

    Add assets and config for assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center">
-      <Text className="text-3xl text-green-700">App router test</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
@@ -13,12 +13,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
